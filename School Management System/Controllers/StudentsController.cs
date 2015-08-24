a72154e16a7ce195dce865b5d24601bbc7ce02b7
Antonio <eltony162@gmail.com>
2015-08-23 21:01:37 -0500

    add

---
+++ School Management System/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace School_Management_System.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        studentDBEntities objapi;
+
+        public StudentsController()
+        {
+            objapi = new studentDBEntities();
+        }
+
+        [HttpGet]
+        public IEnumerable<spConStudents_Result> Get(string StudentName, string StudentEmail)
+        {
+            StudentName = (StudentName == null) ? "" : StudentName;
+            StudentEmail = (StudentEmail == null) ? "" : StudentEmail;
+            return objapi.spConStudents(StudentName, StudentEmail).AsEnumerable();
+        }
+
+        [HttpGet]
+        public IEnumerable<string> insertStudent(string StudentName, string StudentEmail, string Phone, string Address)
+        {
+            return objapi.spInsStudents(StudentName, StudentEmail, Phone, Address).AsEnumerable();
+        }
+
+        [HttpGet]
+        public IEnumerable<string> updateStudent(int stdID, string StudentName, string StudentEmail, string Phone, string Address)
+        {
+            return objapi.spUPDStudents(stdID, StudentName, StudentEmail, Phone, Address).AsEnumerable();
+        }
+
+        [HttpGet]
+        public string deleteStudent(int stdID)
+        {
+            objapi.spDelStudents(stdID);
+            return "deleted";
+        }
+    }
+}
